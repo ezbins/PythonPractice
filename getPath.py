2f17b50a427afad86e1dae1b586fb2daef3a57e8
Dennis <dennis.peng@caretek.com.tw>
2016-07-04 17:59:36 +0800

    wrigint filter function

--- getPath.py
+++ getPath.py
@@ -1,13 +1,43 @@
 #!/usr/bin/env python3
-from os import listdir
-from os.path import isdir, join, basename
+from os import listdir, walk
+from os.path import isdir, join, basename, exists
+
+git_bare_repo = ['branches', 'hooks']
+
+
+def checkFiles(path):
+    for root, dirs, files in walk(path):
+        for filename in files:
+            fullname = join(root, filename)
+            print(fullname, end='\n')
 
 
 def checkDirectory(path):
-    for single_item in listdir(path):
-        fullname = join(path, single_item)
-        if isdir(fullname):
-            print(basename(fullname), end='\n')
+    try:
+        for root, dirs, files in walk(path):
+            for signal_dir in dirs:
+                subpath = join(root, signal_dir)
+                print(type(subpath))
+                subdirs = listdir(subpath)
+                for item in subdirs:
+                    if item in git_bare_repo:
+                         print(subpath, end='\n')
+                         pathlist = dict( subpath= "1")
+                         return pathlist
 
+                        # print(item, end='\n')
+                        # print(subpath, end='\n')
+                # print(subdirs,end='\n')
+                # if signal_dir in git_bare_repo:
+                #      next
+                # else :
+                #      fullDir=join(root,signal_dir)
+                #      print(fullDir,end='\n')
+    except OSError as error:
+        print(error, end='\n')
 
-checkDirectory("/tmp")
+getPath = input("Enter path you wanna looking,with absu path: ")
+mylist=checkDirectory(getPath)
+
+for key, value in mylist.items():
+     print(key,value, end='\n')
