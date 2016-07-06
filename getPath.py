8d7061b81157f502af4c4e7802ea5f2c7ac41249
Dennis <dennis.peng@caretek.com.tw>
2016-07-06 09:52:34 +0800

    取得路徑功能完成

--- getPath.py
+++ getPath.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 from os import listdir, walk
-from os.path import isdir, join, basename, exists
+from os.path import isdir, join, basename, exists,basename
 
 git_bare_repo = ['branches', 'hooks']
 
@@ -12,32 +12,32 @@ def checkFiles(path):
             print(fullname, end='\n')
 
 
+##檢查目錄是否含有'branches','hooks'等目錄.git儲存庫才有的目錄
 def checkDirectory(path):
     try:
         for root, dirs, files in walk(path):
             for signal_dir in dirs:
-                subpath = join(root, signal_dir)
-                print(type(subpath))
+                subpath = join(root, signal_dir)               
                 subdirs = listdir(subpath)
                 for item in subdirs:
-                    if item in git_bare_repo:
-                         print(subpath, end='\n')
-                         pathlist = dict({subpath:1})
-                         return pathlist
-                        
-                        # print(item, end='\n')
-                        # print(subpath, end='\n')
-                # print(subdirs,end='\n')
-                # if signal_dir in git_bare_repo:
-                #      next
-                # else :
-                #      fullDir=join(root,signal_dir)
-                #      print(fullDir,end='\n')
+                    if item in git_bare_repo:                       
+                      pathlist = dict({subpath:1})
+                      return pathlist                       
+                       
     except OSError as error:
         print(error, end='\n')
 
+##將檔案路徑中,取出最後一層目錄的路徑切開
+def splitdirectoryPath(path):
+    for single_path in path:
+        name = basename(single_path)
+        print (name)
+
+
+##執行外部指令
+def excuteCommand():
+    
 getPath = input("Enter path you wanna looking,with absu path: ")
 mylist=checkDirectory(getPath)
 
-for key, value in mylist.items():
-     print(key,value, end='\n')
+splitdirectoryPath(mylist.keys())
