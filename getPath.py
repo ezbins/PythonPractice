2a9e321618e058cb8a2561f473d115ac715de081
Dennis <dennis.peng@caretek.com.tw>
2016-07-07 15:58:31 +0800

    取得目錄路徑功能    *修正取得路徑的方式 執行外部指令     *增加指令完成後,記錄的功能

--- getPath.py
+++ getPath.py
@@ -1,29 +1,25 @@
 #!/usr/bin/env python3
-from os import listdir, walk
+from os import listdir, walk,chdir
 from os.path import isdir, join, basename, exists,basename
 import shlex, subprocess
 
 git_bare_repo = ['branches', 'hooks']
 
-
-def checkFiles(path):
-    for root, dirs, files in walk(path):
-        for filename in files:
-            fullname = join(root, filename)
-            print(fullname, end='\n')
-
-
 ##檢查目錄是否含有'branches','hooks'等目錄.git儲存庫才有的目錄
 def checkDirectory(path):
     try:
+        pathlist={}
         for root, dirs, files in walk(path):
             for signal_dir in dirs:
                 subpath = join(root, signal_dir)               
                 subdirs = listdir(subpath)
                 for item in subdirs:
-                    if item in git_bare_repo:                       
-                      pathlist = dict({subpath:1})
-                      return pathlist                       
+                    if item in git_bare_repo:  
+                      print(subpath)                       
+                      pathlist[subpath] = pathlist.get(subpath,0)+1
+
+        print(pathlist.keys())
+        return pathlist                       
                        
     except OSError as error:
         print(error, end='\n')
@@ -31,25 +27,38 @@ def checkDirectory(path):
 ##將檔案路徑中,取出最後一層目錄的路徑切開
 def splitdirectoryPath(path):
     for singleItem in path:
-        folder = basename(singleItem)
-        absPath = str.split(singleItem,'folder')
+        foldername = basename(singleItem)
+        absPath,folder = str.split(singleItem,foldername)
         ##目錄及路徑分開後,執行備份
-        excuteCommand(absPath,folder)
-        print (name)
+        excuteCommand(absPath,foldername)       
 
 
 ##執行外部指令
-def excuteCommand(path,folder):
-    try:
-        os.chdir(path)
-        command_line="tar zcvf "+folder+".tar.gz "+folder+"\\/"
-        args=shlex.split(command_line)
-        subprocess.Popen(args)
-        
+def excuteCommand(path,foldername):
+    try:       
+        chdir(path)       
+        returnCode=subprocess.call(["tar","-zcvf",foldername+".tar.gz",foldername])
+
+        ##記錄執令指行的結果,
+        if returnCode==0:
+            chdir('/home/shao')
+            logfile=open('tarfile.log','a')
+            logfile.write(foldername+" was achive"+"\n")
+            logfile.close()
+            ##success,writing to log file
+        else:
+            chdir('/var/log')
+            ##fault,writing to fail log file
+            
     except OSError as error:
         print(error)
     
-getPath = input("Enter path you wanna looking,with absu path: ")
-mylist=checkDirectory(getPath)
 
+
+
+getPath = input("Enter path you wanna looking,with absu path: ")
+
+mylist=checkDirectory(getPath)
 splitdirectoryPath(mylist.keys())
+
+
