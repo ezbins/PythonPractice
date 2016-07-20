7026f18f7a4c273ede31042d60b47822e58f28a1
Dennis Peng <ty3627@gmail.com>
2016-07-20 18:02:10 +0800

    建立SVN儲存庫  *改進建立的方式

--- createFolder.py
+++ createFolder.py
@@ -6,16 +6,24 @@ import shutil,subprocess
 
 
 wantedFolder=['RD','Eng']
+projects = ['TWCAXXX130501','TWCA000160401','TWLC001160501','TWCH001160501','TWHO000131101',
+            'TWHO002131101','TWHO002140901','TWCL000151001','TWCH000160701','TWCA001160601']
 try:
-    if(os.path.exists('/home/scm/repositories/svn/si')):
+    if(os.path.exists('/home/scm/repositories/svn/si')):    
         chdir('/home/scm/repositories/svn/si')
-        for folder in wantedFolder:
-            subprocess.call(["svnadmin","create",folder])
+        for parentFolder in projects:
+            for subFolder in wantedFolder:
+                fullPath=str(parentFolder)+'/'+str(subFolder)
+                subprocess.call(["mkdir","-p",fullPath])
+                subprocess.call(["svnadmin","create",fullPath])
     else:
         mkdir('/home/scm/repositories/svn/si')
         chdir('/home/scm/repositories/svn/si')
-        for folder in wantedFolder:
-            subprocess.call(["svnadmin","create",folder])
+        for parentFolder in projects:
+            for subFolder in wantedFolder:
+                fullPath=str(parentFolder)+'/'+str(subFolder)
+                subprocess.call(["mkdir","-p",fullPath])
+                subprocess.call(["svnadmin","create",fullPath])
 
-except OSError:
-    print(OSError.strerror)
+except OSError as e:
+    print(e.strerror)
