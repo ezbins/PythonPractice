d4155f5c61781f97c0a8f73bd7ca74d108afd56f
Dennis Peng <ty3627@gmail.com>
2016-08-15 23:16:49 +0800

    完成main function

--- newNetGat.py
+++ newNetGat.py
@@ -52,8 +52,25 @@ def main():
         if o in ("-h","--help"):
             usage()
         elif o in ("-l","--listen"):
-            usage()
+            listen = True
         elif o in ("-e","--execute"):
-            usage()
+            excute = a
         elif o in ("-c","--commandshell"):
-            usage()    
+           command = True
+        elif o in ("-u","--upload"):
+            upload_destination = a
+        elif o in ("-t","--target"):
+           target = a
+        elif o in ("-p","--port"):
+            port = int(a)
+        else:
+            assert False,"選項未處理"
+
+    if not listen and len(target) and port >0:
+        buffer = sys.stdin.read()
+        client_sender(buffer)
+
+    if listen:
+        server_loop()
+
+    main()
